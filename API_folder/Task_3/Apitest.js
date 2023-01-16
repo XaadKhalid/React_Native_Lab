a27682432c47ef61fc3eb4dba3f5b24c88aab453
XaadKhalid <khalidxaad@gmail.com>
2023-01-16 14:27:59 +0500

    api URL  added

--- API_folder/Task_3/Apitest.js
+++ API_folder/Task_3/Apitest.js
@@ -1,13 +1,18 @@
 /* eslint-disable prettier/prettier */
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Apitest() {
+    useEffect(()=>{
+            fetchData();
+    });
+    const [mydata,setMydata] = useState('saad');
   const fetchData = async () => {
     try {
-      const response = await fetch('');
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
       const data = await response.JSON();
       console.log(data);
+      setMydata(data);
     } catch (error) {
         console.log(error);
     }
@@ -15,6 +20,7 @@ export default function Apitest() {
   return (
     <View>
       <Text>Apitest</Text>
+      <Text>{mydata}</Text>
     </View>
   );
 }
