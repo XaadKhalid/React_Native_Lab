8180d69771f24e1e15fcddc81126a5fe013ce62f
XaadKhalid <khalidxaad@gmail.com>
2023-01-17 13:12:42 +0500

    error resolve in api

--- API_folder/Task_3/Apitest.js
+++ API_folder/Task_3/Apitest.js
@@ -1,26 +1,37 @@
 /* eslint-disable prettier/prettier */
 import {View, Text} from 'react-native';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { Button } from 'react-native-paper';
 
 export default function Apitest() {
-    useEffect(()=>{
-            fetchData();
-    });
-    const [mydata,setMydata] = useState('saad');
   const fetchData = async () => {
     try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-      const data = await response.JSON();
+      const response = await fetch('https://run.mocky.io/v3/51b63b26-493c-46ed-a430-1d3af10721be');
+      const data = await response.json();
       console.log(data);
-      setMydata(data);
+      //setMydata(data);
     } catch (error) {
         console.log(error);
     }
   };
+  // useEffect(() => {
+  //   getData();
+  // },[]);
+  // const getData = ()=>{
+  //   fetch('https://run.mocky.io/v3/51b63b26-493c-46ed-a430-1d3af10721be')
+  //   .then(response=>response.json())
+  //   .then(data=>console.log(data));
+  // };
   return (
     <View>
       <Text>Apitest</Text>
-      <Text>{mydata}</Text>
+      <Button
+            icon="camera"
+            mode="contained"
+            color="#0091EA"
+            onPress={() => fetchData()}>
+            Icon Button
+          </Button>
     </View>
   );
 }
